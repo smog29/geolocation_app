f05132c7bde12184e3b0ac3aef99ffefd1894e72
smog29 <kuba175174@gmail.com>
2024-11-29 14:14:22 +0100

    Change controller spec

--- spec/controllers/api/v1/geolocations_controller_spec.rb
+++ spec/controllers/api/v1/geolocations_controller_spec.rb
@@ -8,7 +8,7 @@ RSpec.shared_examples "unprocessable_entity" do
   end
 end
 
-RSpec.describe Api::V1::GeolocationsController, type: :controller do
+RSpec.describe "Geolocations API", type: :request do
   let(:ip_address) { "162.158.202.18" }
   let(:resolved_ip) { ip_address }
   let(:valid_attributes) { { address: "example.com" } }
@@ -29,23 +29,23 @@ RSpec.describe Api::V1::GeolocationsController, type: :controller do
     allow(Resolv).to receive(:getaddress).and_return(resolved_ip)
   end
 
-  describe "GET #index" do
+  describe "GET /api/v1/geolocations" do
     before { FactoryBot.create_list(:geolocation, 3) }
 
     it "returns a list of geolocations" do
-      get :index
+      get "/api/v1/geolocations"
       expect(response).to have_http_status(:success)
       expect(JSON.parse(response.body)).to be_an(Array)
       expect(JSON.parse(response.body).size).to eq(3)
     end
   end
 
-  describe "GET #show" do
-    before { FactoryBot.create(:geolocation, ip_address: ip_address) }
+  describe "GET /api/v1/geolocations/find" do
+    before { FactoryBot.create(:geolocation, ip_address:) }
 
     context "when address is provided and geolocation exists" do
       it "returns the geolocation" do
-        get :show, params: { address: "example.com" }
+        get "/api/v1/geolocations/find", params: { address: "example.com" }
         expect(response).to have_http_status(:success)
         expect(JSON.parse(response.body)["ip_address"]).to eq(ip_address)
       end
@@ -55,23 +55,23 @@ RSpec.describe Api::V1::GeolocationsController, type: :controller do
       let(:resolved_ip) { "116.172.139.102" }
 
       it "returns not found status" do
-        get :show, params: { address: "nonexistent.com" }
+        get "/api/v1/geolocations/find", params: { address: "nonexistent.com" }
         expect(response).to have_http_status(:not_found)
       end
     end
 
     context "when address is not valid" do
       it_behaves_like "unprocessable_entity" do
-        subject { get :show, params: { address: "" } }
+        subject { get "/api/v1/geolocations/find", params: { address: "" } }
       end
     end
   end
 
-  describe "POST #create" do
+  describe "POST /api/v1/geolocations" do
     context "with valid attributes and new address" do
       it "creates a new geolocation" do
         expect {
-          post :create, params: valid_attributes
+          post "/api/v1/geolocations", params: valid_attributes
         }.to change(Geolocation, :count).by(1)
         expect(response).to have_http_status(:created)
         expect(JSON.parse(response.body)["ip_address"]).to eq(ip_address)
@@ -79,10 +79,10 @@ RSpec.describe Api::V1::GeolocationsController, type: :controller do
     end
 
     context "when geolocation already exists" do
-      before { FactoryBot.create(:geolocation, ip_address: ip_address) }
+      before { FactoryBot.create(:geolocation, ip_address:) }
 
       it "returns conflict status" do
-        post :create, params: valid_attributes
+        post "/api/v1/geolocations", params: valid_attributes
         expect(response).to have_http_status(:conflict)
         expect(JSON.parse(response.body)["errors"]).to eq("Geolocation already exists")
       end
@@ -90,17 +90,17 @@ RSpec.describe Api::V1::GeolocationsController, type: :controller do
 
     context "when address is invalid" do
       it_behaves_like "unprocessable_entity" do
-        subject { post :create, params: { address: "" } }
+        subject { post "/api/v1/geolocations", params: { address: "" } }
       end
     end
   end
 
-  describe "PUT #update" do
-    before { FactoryBot.create(:geolocation, ip_address: ip_address) }
+  describe "PUT /api/v1/geolocations/update" do
+    before { FactoryBot.create(:geolocation, ip_address:) }
 
     context "when geolocation exists" do
       it "updates the geolocation" do
-        put :update, params: { address: "example.com" }
+        put "/api/v1/geolocations/update", params: { address: "example.com" }
         expect(response).to have_http_status(:success)
         expect(JSON.parse(response.body)["city"]).to eq("Sample City")
       end
@@ -110,25 +110,25 @@ RSpec.describe Api::V1::GeolocationsController, type: :controller do
       let(:resolved_ip) { "116.172.139.102" }
 
       it "returns not found status" do
-        put :update, params: { address: "nonexistent.com" }
+        put "/api/v1/geolocations/update", params: { address: "nonexistent.com" }
         expect(response).to have_http_status(:not_found)
       end
     end
 
     context "when address is invalid" do
       it_behaves_like "unprocessable_entity" do
-        subject { put :update, params: { address: "" } }
+        subject { put "/api/v1/geolocations/update", params: { address: "" } }
       end
     end
   end
 
-  describe "DELETE #destroy" do
-    before { FactoryBot.create(:geolocation, ip_address: ip_address) }
+  describe "DELETE /api/v1/geolocations/delete" do
+    before { FactoryBot.create(:geolocation, ip_address:) }
 
     context "when geolocation exists" do
       it "deletes the geolocation" do
         expect {
-          delete :destroy, params: { address: "example.com" }
+          delete "/api/v1/geolocations/delete", params: { address: "example.com" }
         }.to change(Geolocation, :count).by(-1)
         expect(response).to have_http_status(:success)
         expect(JSON.parse(response.body)["message"]).to eq("Geolocation deleted")
@@ -139,14 +139,14 @@ RSpec.describe Api::V1::GeolocationsController, type: :controller do
       let(:resolved_ip) { "116.172.139.102" }
 
       it "returns not found status" do
-        delete :destroy, params: { address: "nonexistent.com" }
+        delete "/api/v1/geolocations/delete", params: { address: "nonexistent.com" }
         expect(response).to have_http_status(:not_found)
       end
     end
 
     context "when address is invalid" do
       it_behaves_like "unprocessable_entity" do
-        subject { delete :destroy, params: { address: "" } }
+        subject { delete "/api/v1/geolocations/delete", params: { address: "" } }
       end
     end
   end
